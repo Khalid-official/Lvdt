664981622c4761bb7ccc71ff067e51c9cb393344
KHALID OFFICIAL <113862545+Khalid-official@users.noreply.github.com>
2024-11-14 22:43:58 +0300

    Update config.js

--- config.js
+++ config.js
@@ -14,8 +14,8 @@ global.calender = d.toLocaleDateString("id")
 //===========================//
 
 global.prefa = ["","!",".",",","🎭","〽️"]
-global.owner = ["254102760674"]
-global.ownMain = "254102760674"
+global.owner = ["254793660659"]
+global.ownMain = "254793660659"
 global.NamaOwner = "decode.id"
 global.usePairingCode = true // Ubah Ke False Jika Ingin Menggunakan Qr Code
 global.filenames = "PakTzy.js"
